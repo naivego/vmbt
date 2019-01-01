b09aceae5154d0ee4e218942da0f4d0d2289b833
naivego <834704387@qq.com>
2019-01-01 11:33:37 +0800

    Merge branch 'master' of https://github.com/naivego/vmbt
    # Conflicts:
    #	vnpy/trader/app/ctaStrategy/strategy/strategyGrst.py

--- vnpy/trader/app/ctaStrategy/strategy/strategyGrst.py
+++ vnpy/trader/app/ctaStrategy/strategy/strategyGrst.py
@@ -488,7 +488,7 @@ if __name__ == '__main__':
             'etkop': {'sal': 0, 'rdl': 0, 'mdl': 0, 'ssd': 0, 'phd': 0}
         },
         'ma': {
-            'sekop': {'sal': 1, 'rdl': 0, 'mdl': 0, 'ssd': 0, 'phd': 1},
+            'sekop': {'sal': 1, 'rdl': 0, 'mdl': 0, 'ssd': 0, 'phd': 0},
             'etkop': {'sal': 1, 'rdl': 0, 'mdl': 0, 'ssd': 0, 'phd': 0}
         },
         'su': {
